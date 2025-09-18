ee84bc5bc292edc6d5b80e4e9d5c3abca461a3d4
sidhant yadav <sidhant2yadav@gmail.com>
2025-09-18 10:38:29 +0530

    Add superuser creation

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Create superuser if it doesn't exist
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'your_password_here')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'sid22')" | python manage.py shell
